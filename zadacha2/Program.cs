7f1440d0666507aab6dada29c495e3ffe737c788
Temirlan Madykhan <tima.001@mail.ru>
2023-02-04 19:22:03 +0500

    HomeWork3

---
+++ zadacha2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты точки x1, y1, z1");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки x2, y2, z2");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+
+double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+Console.WriteLine($"{result}");
---
+++ zadacha3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= N)
+{
+    int result = (int) Convert.ToInt32(Math.Pow(index, 3));
+    Console.WriteLine(result);
+    index++;
+}
